2ba0de7fcc10cf53405b0af4dd47836ad0291ece
networkoctopus <aidan@networkoctopus.com>
2026-05-12 12:48:50 +0100

    revert backlight change

--- build_files/fixes/restore-backlight.sh
+++ build_files/fixes/restore-backlight.sh
@@ -5,14 +5,8 @@ case "$1" in
         cat "$BACKLIGHT/brightness" > /tmp/backlight-save 2>/dev/null || true
         ;;
     post)
-        start=$(date +%s%3N)
-        deadline=$((start + 3000))
-        until [ -f /tmp/backlight-save ] && \
-            cat /tmp/backlight-save > "$BACKLIGHT/brightness" 2>/dev/null; do
-            sleep 0.05
-            [ $(date +%s%3N) -ge $deadline ] && break
-        done
-        end=$(date +%s%3N)
-        echo "$((end - start))ms wait for backlight" >> /etc/backlight-resume.log
+        sleep 1
+        [ -f /tmp/backlight-save ] && \
+            cat /tmp/backlight-save > "$BACKLIGHT/brightness" 2>/dev/null || true
         ;;
 esac

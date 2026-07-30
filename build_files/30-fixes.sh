eda433719e70c1c33e7f18ab74d9f509bc8491f0
networkoctopus <77534662+networkoctopus@users.noreply.github.com>
2026-07-30 13:23:27 +0100

    add resume fix as per https://github.com/Dunedan/mbp-2016-linux/issues/37#issuecomment-540202102

--- build_files/30-fixes.sh
+++ build_files/30-fixes.sh
@@ -7,3 +7,12 @@ set -ouex pipefail
 install -Dm755 /ctx/fixes/fix-macbook-wakeup \
     /usr/lib/systemd/system-sleep/fix-macbook-wakeup
 
+# Disable d3cold for the SATA controller to fix resume hang on MacBookAir7,1
+# Ref: https://github.com/Dunedan/mbp-2016-linux/issues/37#issuecomment-540202102
+install -Dm755 /ctx/fixes/fix-sleep.sh \
+  /usr/lib/systemd/scripts/fix-sleep.sh
+
+install -Dm644 /ctx/fixes/fix-sleep.service \
+  /usr/lib/systemd/system/fix-sleep.service
+
+systemctl enable fix-sleep.service

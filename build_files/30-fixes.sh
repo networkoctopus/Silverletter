403707c013398fdcdf65d90836aea67ad264fdbc
networkoctopus <77534662+networkoctopus@users.noreply.github.com>
2026-08-03 15:01:04 +0100

    use iwd as backend for wireless

--- build_files/30-fixes.sh
+++ build_files/30-fixes.sh
@@ -7,6 +7,15 @@ set -ouex pipefail
 install -Dm755 /ctx/fixes/fix-macbook-wakeup \
     /usr/lib/systemd/system-sleep/fix-macbook-wakeup
 
+### Use iwd as NetworkManager's WiFi backend, with broadcom-wl tuning
+dnf5 install -y iwd
+install -Dm644 /ctx/fixes/10-wifi-backend.conf \
+    /usr/lib/NetworkManager/conf.d/10-wifi-backend.conf
+install -Dm644 /ctx/fixes/main.conf \
+    /usr/lib/iwd/main.conf
+install -Dm644 /ctx/fixes/10-silverletter-config.conf \
+    /usr/lib/systemd/system/iwd.service.d/10-silverletter-config.conf
+
 ### Broadcom wl WiFi interface reset on suspend/resume
 #install -Dm644 /ctx/fixes/wl-suspend.service /usr/lib/systemd/system/wl-suspend.service
 #install -Dm755 /ctx/fixes/wl-suspend.sh /usr/bin/wl-suspend.sh
